8ab6b59a3362dfd66f0627060a10340719cb457b
DESKTOP-JOJ5L1Q\82109 <c5ngdp@gmail.com>
2023-07-25 20:28:02 +0900

    Section 4 - 회원 레포지토리 개발

---
+++ jpashop/src/main/java/inflearn/springboot/jpashop/repository/MemberRepository.java
@@ -0,0 +1,34 @@
+package inflearn.springboot.jpashop.repository;
+
+import inflearn.springboot.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em; /* Srping이 EM을 만들어 DI 해준다. */
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList(); /* 첫 번째 파라미터는 JPQL, 두 번째 파라미터는 반환 타입, JPQL은 Entity 객체에 대한 문법 */
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
